231da1a8ca2a1522ebc2a73481ce79c6300899c5
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2021-02-11 15:49:49 +0100

    15th commit - concerning the new website version - on branch main - Testing Travis

--- Pure_Beurre/substitute/management/commands/tests.py
+++ Pure_Beurre/substitute/management/commands/tests.py
@@ -21,5 +21,6 @@ def native_shell_script():
     os.system("./Pure_Beurre/manage.py test ./Pure_Beurre/substitute/project_tester/tests.py")
     os.system("coverage run --source='.' ./Pure_Beurre/manage.py test substitute.project_tester.selenium substitute.project_tester.tests")
     os.system("coverage html --skip-covered --skip-empty -d substitute\project_tester\coverage_html")
+    os.system("sensible-browser substitute\project_tester\coverage_html\index.html")
     os.system("start substitute\project_tester\coverage_html\index.html")
 
