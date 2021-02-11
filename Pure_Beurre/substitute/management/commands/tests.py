51e35daed3cfab5883ef0aa581f9780ba0030c05
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2021-02-11 14:47:54 +0100

    10th commit - concerning the new website version - on branch main - Testing Travis

--- Pure_Beurre/substitute/management/commands/tests.py
+++ Pure_Beurre/substitute/management/commands/tests.py
@@ -1,10 +1,23 @@
 from django.core.management.base import BaseCommand
-import subprocess
+import subprocess, os
 
 class Command(BaseCommand):
     print("Préparation de la campagne de tests")
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Lancement de la campagne de tests..."))
-        subprocess.call([r'start_test.bat'])
-        subprocess.call([r'start_test.sh'])
+        try:
+            native_shell_script()
+        except Exception as e:
+            print(e)
         self.stdout.write(self.style.SUCCESS("Fin de la campagne de tests..."))
+
+def batch_script():
+    subprocess.call([r'start_test.bat'])
+
+def shell_script():
+    subprocess.call([r'start_test.sh'])
+
+def native_shell_script():
+    os.system("coverage run --source='.' manage.py test substitute.project_tester.selenium substitute.project_tester.tests")
+    os.system("coverage html --skip-covered --skip-empty -d substitute\project_tester\coverage_html")
+    os.system("start substitute\project_tester\coverage_html\index.html")
