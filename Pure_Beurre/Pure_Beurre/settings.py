050b078ff97d3aa582cc22d8785fa8b45921645e
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2021-02-11 16:19:36 +0100

    17th commit - concerning the new website version - on branch main - Testing Travis

--- Pure_Beurre/Pure_Beurre/settings.py
+++ Pure_Beurre/Pure_Beurre/settings.py
@@ -90,16 +90,29 @@ WSGI_APPLICATION = 'Pure_Beurre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'purebeurre',
+#        'USER': 'purebeurre',
+#        'PASSWORD': 'purebeurreoc',
+#        'HOST': 'localhost',
+#        'PORT': '',
+#    }
+#}
+
+# This database configuration is used by Travis
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'purebeurre',
-        'USER': 'purebeurre',
-        'PASSWORD': 'purebeurreoc',
-        'HOST': 'localhost',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '',
-    }
+    },
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
