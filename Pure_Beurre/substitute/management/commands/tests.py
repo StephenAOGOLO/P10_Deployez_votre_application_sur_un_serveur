56f64bbd792646143de28b0567f58309296606b1
stephenAOGOLO <stephen.adams.ogolo@gmail.com>
2021-02-15 16:42:25 +0100

    57th commit - concerning the new website version - on branch main - Testing Travis

--- Pure_Beurre/substitute/management/commands/tests.py
+++ Pure_Beurre/substitute/management/commands/tests.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS("Lancement de la campagne de tests..."))
         try:
-            selenium_tests()
+            all_tests()
         except Exception as e:
             print(e)
         self.stdout.write(self.style.SUCCESS("Fin de la campagne de tests..."))
